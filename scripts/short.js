fc7ca2625b51b3df6b8af2bc97268acbeab07479
0x-stan <744428333@qq.com>
2022-05-08 18:33:12 +0800

    calc profit

--- scripts/short.js
+++ scripts/short.js
@@ -17,8 +17,12 @@ const {
   revertNetwork,
   setBalance,
   impersonateAccount,
+  mineBlock,
 } = require("../utils/fork-utils");
-const { hugeSwapToChangePrice } = require("../utils/uniswap-utils");
+const {
+  hugeSwapToChangePrice,
+  checkSwapPrice,
+} = require("../utils/uniswap-utils");
 
 const {
   abi: ETokenAbi,
@@ -27,11 +31,15 @@ const {
   abi: DTokenAbi,
 } = require("@eulerxyz/euler-interfaces/abis/modules/DToken.json");
 
-const whaleWETH = "0xf584F8728B874a6a5c7A8d4d387C9aae9172D621"; // WETH whale
-const whaleUSDC = "0x7d812B62Dc15e6F4073ebA8a2bA8Db19c4E40704"; // DAI whale
-const whaleDAI = "0x7c8CA1a587b2c4c40fC650dB8196eE66DC9c46F4"; // DAI whale
-const whaleUNI = "0x50796bA0F82EeF414d7b609BbDD8a5c9a785e77d"; // UNI whale
+const CONFIG_FACTOR_SCALE = ethers.BigNumber.from("4000000000");
+const ONE = parseEther("1");
 
+const WHALES = {
+  WETH: "0xf584F8728B874a6a5c7A8d4d387C9aae9172D621",
+  USDC: "0x7d812B62Dc15e6F4073ebA8a2bA8Db19c4E40704",
+  DAI: "0x7c8CA1a587b2c4c40fC650dB8196eE66DC9c46F4",
+  UNI: "0x50796bA0F82EeF414d7b609BbDD8a5c9a785e77d",
+};
 class Trader {
   constructor(user, userAddr, principalAmount, tokenShortOn, tokenAgainst) {
     this.user = user;
@@ -76,14 +84,13 @@ class Trader {
     this.execSinger = exec.connect(this.user);
   }
 
-  async prepareToken(whale) {
+  async prepareToken(whale, token = this.tokenAgainst) {
     await setBalance(whale);
     const userAddr = this.userAddr;
-    const tokenAgainst = this.tokenAgainst;
     const amount = this.principalAmount;
 
     await impersonateAccount(whale, async function (signer) {
-      const tokenSinger = tokenAgainst.connect(signer);
+      const tokenSinger = token.connect(signer);
       await (await tokenSinger.approve(whale, MaxUint256)).wait();
       await (await tokenSinger.transferFrom(whale, userAddr, amount)).wait();
 
@@ -94,21 +101,23 @@ class Trader {
     });
   }
 
-  async depositAction() {
-    await this.enterMarket(this.tokenAgainst);
+  async depositAction(symbol = "against") {
+    let token, EToken;
+    if (symbol === "against") {
+      token = this.tokenAgainst;
+      EToken = this.ETokenAgainst;
+    } else {
+      token = this.tokenShortOn;
+      EToken = this.ETokenShortOn;
+    }
+    await (await token.approve(euler.address, MaxUint256)).wait();
 
-    await (await this.tokenAgainst.approve(euler.address, MaxUint256)).wait();
-
-    await (await this.ETokenAgainst.deposit(0, this.principalAmount)).wait();
+    await (await EToken.deposit(0, this.principalAmount)).wait();
 
     console.log(
       "\ndeposit get EToken ",
-      (
-        await this.ETokenAgainst.connect(this.user).balanceOf(this.userAddr)
-      ).toString()
+      (await EToken.balanceOf(this.userAddr)).toString()
     );
-
-    return this.ETokenAgainst;
   }
 
   async enterMarket(underlying) {
@@ -131,30 +140,26 @@ class Trader {
   async openShort(leverage) {
     // 1. mint shortOn assets, get EToken and DToken of shortOn
     // 2. swap shortOn EToken to against EToken
-    console.log("leverage", leverage);
+    console.log("leverage", leverage / 10000);
 
     const { collateralValue } = await this.checkAssetsValue();
     const { borrowFactor } =
       await this.marketSigner.underlyingToAssetConfigUnresolved(
         this.tokenShortOn.address
       );
-    const priceShortOn = await this.checkPrice(this.tokenShortOn);
+    const priceShortOn = await this.checkPrice(this.tokenShortOn, true);
     let mintAmount = collateralValue
-      .mul(parseEther("1"))
+      .mul(ONE)
       .mul(leverage)
       .div(priceShortOn)
       .mul(borrowFactor)
-      .div("4000000000");
-
-    // mintAmount = parseEther('10')
+      .div(CONFIG_FACTOR_SCALE)
+      .div(10000);
 
     console.log("mintAmount", mintAmount / 1e18);
 
     console.log("\nbefore mint");
     this.checkLiquidity(true);
-
-    await this.enterMarket(this.tokenShortOn);
-    await this.enterMarket(this.tokenAgainst);
 
     // batchDispatch with deferLiquidityChecks
     let batchIterms = [
@@ -177,7 +182,7 @@ class Trader {
             subAccountIdOut: 0,
             underlyingIn: this.tokenShortOn.address,
             underlyingOut: this.tokenAgainst.address,
-            amountIn: MaxUint256,
+            amountIn: mintAmount,
             amountOutMinimum: 0,
             deadline: 0,
             fee: 3000,
@@ -190,43 +195,52 @@ class Trader {
       await this.execSinger.batchDispatch(batchIterms, [this.userAddr])
     ).wait();
 
+    console.log(
+      "ETokenAgainst balance",
+      await this.ETokenAgainst.balanceOfUnderlying(this.userAddr)
+    );
+
     console.log("\nafter mint");
     // this.checkLiquidity(true);
   }
 
   async closeShort() {
-    // 1. swap against EToken to shortOn EToken
-    // 1. burn shortOn EToken and DToken
+    // 1. swap Againgst-EToken to ShortOn-EToken
+    // 2. burn shortOn EToken and DToken
 
     // batchDispatch with deferLiquidityChecks
-    const amountOut = await this.DTokenShortOn.balanceOf(this.userAddr);
-    console.log("amountOut", amountOut);
+    let amount = await this.ETokenAgainst.balanceOfUnderlying(this.userAddr);
+    // let amount = await this.DTokenShortOn.balanceOf(this.userAddr);
+    console.log("swap amount", amount);
+
+    this.checkLiquidity(true);
+
     let batchIterms = [
       // swap Againgst-EToken to ShortOn-EToken
       {
         allowError: false,
         proxyAddr: swap.address,
-        data: swap.interface.encodeFunctionData("swapUniExactOutputSingle", [
+        data: swap.interface.encodeFunctionData("swapUniExactInputSingle", [
           {
             subAccountIdIn: 0,
             subAccountIdOut: 0,
             underlyingIn: this.tokenAgainst.address,
             underlyingOut: this.tokenShortOn.address,
-            amountOut: amountOut.mul(1000).div(997),
-            amountInMaximum: MaxUint256,
+            amountIn: amount,
+            amountOutMinimum: 0,
             deadline: 0,
             fee: 3000,
             sqrtPriceLimitX96: 0,
           },
         ]),
       },
-      // burn UNI-EToken and UNI-DToken
+      // burn ShortOn-EToken and ShortOn-DToken
       {
         allowError: false,
         proxyAddr: this.ETokenShortOnAddr,
         data: this.ETokenShortOn.interface.encodeFunctionData("burn", [
           0,
-          amountOut,
+          MaxUint256,
         ]),
       },
     ];
@@ -239,9 +253,12 @@ class Trader {
   }
 
   async checkPrice(underlying, log = false) {
-    const res = await exec.connect(this.user).getPrice(underlying.address);
+    const res = await this.execSinger.getPriceFull(underlying.address);
     const symbol = await underlying.connect(this.user).symbol();
-    if (log) console.log(`${symbol} twap: ${res[0].toString()}`);
+    if (log)
+      console.log(
+        `${symbol} twap: ${res[0] / 1e18} curPrice: ${res[2] / 1e18} price2: ${ONE.mul(ONE).div(res[0])  / 1e18}`
+      );
     return res[0];
   }
 
@@ -254,16 +271,11 @@ class Trader {
   async checkAssetsValue(log = false) {
     const { collateralValue, liabilityValue, numBorrows, borrowIsolated } =
       await this.checkLiquidity(false);
-    const tokenShortOnPrice = await this.checkPrice(this.tokenShortOn, false);
-    const tokenAgainstPrice = await this.checkPrice(this.tokenAgainst, false);
+    const priceShortOn = await this.checkPrice(this.tokenShortOn, false);
+    const priceAgainst = await this.checkPrice(this.tokenAgainst, false);
 
-    let DTokenShortOnValue = (await this.DTokenShortOn.balanceOf(this.userAddr))
-      .mul(tokenShortOnPrice)
-      .div(parseEther("1"));
-    let DTokenAgainstValue = (await this.DTokenAgainst.balanceOf(this.userAddr))
-      .mul(tokenAgainstPrice)
-      .div(parseEther("1"));
-
+    let DTokenShortOnValue = await this.DTokenShortOn.balanceOf(this.userAddr);
+    let DTokenAgainstValue = await this.DTokenAgainst.balanceOf(this.userAddr);
     let BalanceShortOn = await this.ETokenShortOn.balanceOfUnderlying(
       this.userAddr
     );
@@ -278,20 +290,16 @@ class Trader {
       await this.tokenAgainst.balanceOf(this.userAddr)
     );
 
-    const tokenShortOnValue = BalanceShortOn.mul(tokenShortOnPrice).div(
-      parseEther("1")
-    );
-    const tokenAgainstValue = BalanceAgainst.mul(tokenAgainstPrice).div(
-      parseEther("1")
-    );
+    const tokenShortOnValue = BalanceShortOn.mul(priceShortOn).div(ONE);
+    const tokenAgainstValue = BalanceAgainst.mul(priceAgainst).div(ONE);
 
     const totalValue = tokenShortOnValue
       .add(tokenAgainstValue)
-      .sub(DTokenShortOnValue)
-      .sub(DTokenAgainstValue);
+      .sub(DTokenShortOnValue.mul(priceShortOn).div(ONE))
+      .sub(DTokenAgainstValue.mul(priceAgainst).div(ONE));
 
     const healthScore = liabilityValue.gt(ethers.BigNumber.from(0))
-      ? collateralValue.mul(parseEther("1")).div(liabilityValue) / 1e18
+      ? collateralValue.mul(ONE).div(liabilityValue) / 1e18
       : "max";
 
     if (log)
@@ -336,20 +344,21 @@ async function main() {
   await shortTrader.installSinger();
 
   try {
-    await shortTrader.prepareToken(whaleWETH);
+    await shortTrader.prepareToken(WHALES["WETH"], shortTrader.tokenAgainst);
 
-    await shortTrader.depositAction();
+    await shortTrader.enterMarket(shortTrader.tokenShortOn);
+    await shortTrader.enterMarket(shortTrader.tokenAgainst);
 
-    // Now the user tries to mint an amount X of ShortOn assset.
-    // Since the self-collateralisation factor is 0.95, then X * .95 of this mint is self-collateralised.
-    // The remaining 5% is a regular borrow that is adjusted up by the BF of 0.6:
-    //     liability = X * (1 - 0.95) / 0.6
-    // Using a risk-adjusted value of 0.375, we can solve for the maximum allowable X:
-    //     0.375 = X * (1 - 0.95) / 0.6
-    //     X = 4.5
+    await shortTrader.depositAction("against");
+    // await shortTrader.depositAction("shortOn");
 
-    const leverage = 3;
+    await shortTrader.checkAssetsValue(true);
+
+    // leverage base = 10000
+    const leverage = 30000;   // max leverage is 3.001 (?)
     await shortTrader.openShort(leverage);
+
+    console.log("open swap price", await checkSwapPrice());
 
     const { totalValue } = await shortTrader.checkAssetsValue(true);
 
@@ -359,37 +368,50 @@ async function main() {
     );
 
     await hugeSwapToChangePrice(
-      whaleUNI,
+      WHALES["UNI"],
       shortTrader.tokenShortOn,
-      WETH,
-      parseEther("500000"),
-      100,
+      shortTrader.tokenAgainst,
+      parseEther("80000"),
+      600,
       1
     );
+
+    // await shortTrader.checkAssetsValue(true);
+
+    // await shortTrader.checkPrice(shortTrader.tokenShortOn, true);
+
+    // console.log(
+    //   `\nprofit ${
+    //     ((await shortTrader.checkAssetsValue(true)).totalValue
+    //       .mul(ONE)
+    //       .div(totalValue) /
+    //       1e18 -
+    //       1) *
+    //     100
+    //   }%`
+    // );
+
+    // await shortTrader.closeShort();
+    // console.log("close swap price", await checkSwapPrice());
 
     const priceAfter = await shortTrader.checkPrice(
       shortTrader.tokenShortOn,
       true
     );
+    const totalValueAfter = (await shortTrader.checkAssetsValue(true))
+      .totalValue;
+    const priceDiff = priceBefore.sub(priceAfter);
 
     console.log(
-      `price changed ${
-        priceBefore.sub(priceAfter).mul(parseEther("100")).div(priceBefore) /
-        1e18
-      }%`
+      `price changed ${(priceDiff.mul(ONE).div(priceBefore) / 1e18) * 100}%`
     );
-    await shortTrader.checkAssetsValue(true);
 
-    // await shortTrader.closeShort();
-
-    const profitPersent = (await shortTrader.checkAssetsValue(true)).totalValue
-      .mul(parseEther("1"))
-      .div(totalValue);
-
+    let profitPersent = totalValueAfter.mul(ONE).div(totalValue);
     console.log(`\nprofit ${(profitPersent / 1e18 - 1) * 100}%`);
   } catch (error) {
     console.error(error);
     await revertNetwork(snapshot);
+    process.exit(0);
   }
 
   await revertNetwork(snapshot);
--- utils/uniswap-utils.js
+++ utils/uniswap-utils.js
@@ -8,6 +8,7 @@ const {
 } = require("@uniswap/swap-router-contracts/artifacts/contracts/SwapRouter02.sol/SwapRouter02.json");
 const { UNI } = require("./loadContracts");
 const V3_SWAP_ROUTER_ADDRESS = "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45";
+const ONE = ethers.utils.parseEther("1");
 
 async function hugeSwapToChangePrice(
   whale,
@@ -17,7 +18,7 @@ async function hugeSwapToChangePrice(
   duration = 100,
   swapTimes = 4
 ) {
-  await mineBlock(20);
+  await mineBlock(1);
   await impersonateAccount(whale, async function (signer) {
     await setBalance(whale);
     await (
@@ -46,12 +47,11 @@ async function hugeSwapToChangePrice(
       .div(ethers.utils.parseEther("1"));
     const timeInterval = Math.ceil(duration / swapTimes);
     for (let i = 0; i < swapTimes; i++) {
-      await mineBlock(timeInterval);
       const _amount = amountSplit.gt(amountIn) ? amountIn : amountSplit;
       await (
         await router.exactInput({
           path: path,
-          recipient: await signer.getAddress(),
+          recipient: await signer.getAddress(signer),
           deadline: Math.floor(Date.now() / 1000 + 1800),
           amountIn: _amount,
           amountOutMinimum: 0,
@@ -59,13 +59,34 @@ async function hugeSwapToChangePrice(
       ).wait();
       amountIn = amountIn.sub(amountSplit);
       console.log(`hugeSwapToChangePrice exactInput ${_amount.toString()}`);
+      console.log("huge swap price", await checkSwapPrice());
+
+      await mineBlock(timeInterval);
     }
-
-
   });
-  await mineBlock(20);
+  await mineBlock(600);
+}
+
+async function checkSwapPrice(provider = ethers.provider) {
+  const abi = [
+    "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)",
+  ];
+  const uniV3Pool = new ethers.Contract(
+    "0x1d42064Fc4Beb5F8aAF85F4617AE8b3b5B8Bd801",   // UNI-WETH UniswapV3Pool
+    abi,
+    provider
+  );
+  const filters = uniV3Pool.filters.Swap();
+  let log = await provider.getLogs(filters);
+
+  if (log.length === 0) return 0;
+
+  log = uniV3Pool.interface.parseLog(log[log.length - 1]);
+  // console.log("sqrtPriceX96", log.args.sqrtPriceX96)
+  return log.args.sqrtPriceX96.mul(log.args.sqrtPriceX96).div(ONE);
 }
 
 module.exports = {
   hugeSwapToChangePrice,
+  checkSwapPrice,
 };
